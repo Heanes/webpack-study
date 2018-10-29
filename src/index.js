06fcbf4c982db859eb3470054b343d4b1c3daaa3
Heanes <heanes@163.com>
2018-10-29 23:05:47 +0800

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './logo-on-white-bg.png';
 
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
