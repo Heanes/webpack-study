dbaa7f985eed03740b5e4f12b460e6abc56805e1
Heanes <heanes@163.com>
2018-10-30 17:02:15 +0800

    多入口

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo-on-white-bg.png';
+import printMe from './print';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
