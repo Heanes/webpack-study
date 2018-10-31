8c88f4af0aa1483abf9e2691ebbbd13573e0d953
Heanes <heanes@163.com>
2018-10-31 17:24:05 +0800

    use hot module replacement

--- src/print.js
+++ src/print.js
@@ -2,4 +2,5 @@ export default function printMe() {
     console.log('I get called from print.js');
     console.log('watched');
     console.log('webpack-dev-server start');
+    console.log('Updating print.js...');
 }
